688044d7168adda173f75243eed627f1ba160867
Peter-32 <peter@impactradius.com>
2021-05-20 00:09:13 -0700

    init

---
+++ swift-mvc/Model/AppleProduct.swift
@@ -0,0 +1,18 @@
+//
+//  AppleProduct.swift
+//  swift-mvc
+//
+//  Created by Peter Myers on 5/20/21.
+//
+
+import Foundation
+
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        
+    }
+}
---
+++ swift-mvc/swift_mvcApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_mvcApp.swift
+//  swift-mvc
+//
+//  Created by Peter Myers on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_mvcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
